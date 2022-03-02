7d7f1bfed10f88862afbc9da6972b3f209311cbe
Victor Ndaba <mwangindaba@gmail.com>
2022-03-02 23:55:29 +0300

    feat(src/utils): added utilities modules

---
+++ src/utils/listeners.rs
@@ -0,0 +1,11 @@
+use super::super::{Event, Program};
+
+pub fn check_for_listener(event: Event, program: &Program, param: String) {
+    if program.listeners.contains_key(&event) {
+        let callbacks = program.listeners.get(&event).unwrap();
+        for cb in callbacks {
+            cb(program, param.clone())
+        }
+    }
+    std::process::exit(1)
+}
---
+++ src/utils/mod.rs
@@ -0,0 +1,3 @@
+pub mod listeners;
+
+pub use listeners::check_for_listener;
