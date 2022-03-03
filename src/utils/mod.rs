c81fdec138bbb7ffbbaca7f57196cf19e3a1b18c
Victor Ndaba <mwangindaba@gmail.com>
2022-03-03 16:33:10 +0300

    feat(src/utils): included fn for separatin args into flags

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,4 +2,4 @@ pub mod listeners;
 pub mod params;
 
 pub use listeners::check_for_listener;
-pub use params::clean_arg;
+pub use params::{clean_arg, filter_flags};
--- src/utils/params.rs
+++ src/utils/params.rs
@@ -1,12 +1,17 @@
+use crate::parser::{Cmd, Flag};
+
 use super::super::parser::commands::Argument;
 
 pub fn clean_arg(val: &str) -> Argument {
     let delimiters: &str;
+    let required: bool;
 
     if val.starts_with('<') {
-        delimiters = "< >"
+        delimiters = "< >";
+        required = true;
     } else {
-        delimiters = "[ ]"
+        delimiters = "[ ]";
+        required = false;
     }
 
     let vals: Vec<_> = delimiters.split(' ').collect();
@@ -16,7 +21,53 @@ pub fn clean_arg(val: &str) -> Argument {
     let name = val.replace(start, "").replace(last, "").replace('-', "_");
     Argument {
         name,
-        required: true,
+        required,
         literal: val.to_string(),
     }
+}
+
+pub fn filter_flags(cmd: &Cmd, args: &Vec<String>) -> (Vec<String>, Vec<String>) {
+    let mut flags = vec![];
+    let mut vals = vec![];
+    for (i, a) in args.iter().enumerate() {
+        if a.starts_with('-') {
+            flags.push(a.clone());
+            match get_flag(a, cmd) {
+                Some(f) => {
+                    let _len = f.params.len();
+                    for (idx, p) in f.params.iter().enumerate() {
+                        if p.required {
+                            flags.push(args[i + (idx + 1)].clone())
+                        } else {
+                            if !args[i + 1].starts_with('-') {
+                                flags.push(args[i + (idx + 1)].clone())
+                            }
+                        }
+                    }
+                }
+                None => {}
+            }
+        }
+    }
+
+    for a in args {
+        if !flags.contains(a) {
+            vals.push(a.clone())
+        }
+    }
+    (flags, vals)
+}
+
+fn get_flag(val: &str, cmd: &Cmd) -> Option<Flag> {
+    let mut ans: Option<Flag> = None;
+
+    for f in &cmd.options {
+        if val == f.short || val == f.long {
+            ans = Some(f.clone());
+        } else {
+            ans = None;
+        }
+    }
+
+    ans
 }
