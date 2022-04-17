48b28a5cf3e63a4331c9e43ba1c2e929e1700a0e
Victor Ndaba <mwangindaba@gmail.com>
2022-04-17 20:41:42 +0300

    🔧 fix: handled empty args passed to parser case

--- src/parser/core_parser.rs
+++ src/parser/core_parser.rs
@@ -20,6 +20,14 @@ impl<'a> Parser<'a> {
         parent: &str,
         raw_args: &[String],
     ) -> (HashMap<String, String>, HashMap<String, String>) {
+        if raw_args.is_empty() {
+            match parent {
+                "cmd" => self.cmd.unwrap().output_command_help(self.program, ""),
+                _ => self.program.output_help(""),
+            }
+            std::process::exit(1);
+        }
+
         let mut values = HashMap::new();
         let mut options = HashMap::new();
 
