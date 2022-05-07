53f4b4bb85a0db2b84a51caa046aab85f5bd7df3
Victor Ndaba <mwangindaba@gmail.com>
2022-05-07 23:21:26 +0300

    📦 feat: implemented Into String trait for errors

--- src/core/errors.rs
+++ src/core/errors.rs
@@ -10,34 +10,40 @@ pub enum CmderError<'err> {
     UnknownOption(&'err str),           // exit code 15
 }
 
+impl<'a> Into<String> for CmderError<'a> {
+    fn into(self) -> String {
+        match self {
+            CmderError::MissingArgument(ref val) => {
+                let arg_string = get_vector_string(val);
+                format!("Missing the following required argument(s): {arg_string}")
+            }
+            CmderError::OptionMissingArgument(ref args) => {
+                format!(
+                    "Missing required argument(s): `{}` for option: `{}`",
+                    args[0], args[1]
+                )
+            }
+            CmderError::UnknownCommand(cmd) => {
+                format!("Could not find command: `{cmd}`")
+            }
+            CmderError::UnknownOption(opt) => {
+                format!("You have passed an unknown option: `{opt}`")
+            }
+        }
+    }
+}
+
 impl<'a> fmt::Display for CmderError<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
-        match *self {
-            CmderError::MissingArgument(ref args) => {
-                let arg_string = get_vector_string(args);
-                f.write_fmt(format_args!(
-                    "Missing the following required argument(s): {arg_string}"
-                ))
-            }
-            CmderError::OptionMissingArgument(ref args) => f.write_fmt(format_args!(
-                "Missing required argument(s): {} for option: {}",
-                args[0], args[1]
-            )),
-            CmderError::UnknownOption(ref opt) => {
-                f.write_fmt(format_args!("You have passed an unknown option: {opt}"))
-            }
-            CmderError::UnknownCommand(ref cmd) => {
-                f.write_fmt(format_args!("Could not find command: {cmd}"))
-            }
-            _ => f.write_str("An error occurred"),
-        }
+        let val: String = self.to_owned().into();
+        f.write_str(&val)
     }
 }
 
 fn get_vector_string(args: &Vec<String>) -> String {
     let mut res = String::new();
     for a in args {
-        res.push_str(a.as_str());
+        res.push_str(&format!("`{a}`"));
         res.push(' ');
     }
 
