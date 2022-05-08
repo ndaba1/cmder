14254cfdebce4a9c0f9263c778765717b643a44c
Victor Ndaba <mwangindaba@gmail.com>
2022-05-08 08:46:50 +0300

    📦 feat: added settings examples for the new api

--- examples/new_api.rs
+++ examples/new_api.rs
@@ -1,4 +1,10 @@
-use cmder::core::new_program::Program;
+#![allow(unused)]
+
+use cmder::{
+    construct_theme,
+    core::{new_program::Program, Setting},
+    Color, Event, ParserMatches, Pattern, PredefinedThemes, Theme,
+};
 
 fn main() {
     let mut program = Program::new();
@@ -16,7 +22,7 @@ fn main() {
         .description("A test subcommand")
         .option("-a --all", "Run all the configured tests")
         .option("-q --quiet", "Don't show tests output")
-        .build(program.s());
+        .build();
 
     program
         .subcommand("image")
@@ -25,20 +31,46 @@ fn main() {
         .description("A command to start up images")
         .option("-p --port <port-number>", "The port to start the image on")
         .option("-a -all", "Perform the action for all images")
-        .build(program.s());
+        .action(image_cmd_cb)
+        .build();
 
     let mut temp_cmd = program.subcommand("temp");
 
     temp_cmd
         .subcommand("hello")
         .description("Some desc")
-        .build(temp_cmd.s());
+        .build();
 
     temp_cmd
         .alias("tmp")
         .description("A command with subcmds")
-        .build(program.s());
+        .build();
+
+    // program.set(Setting::EnableCommandSuggestion(false));
+    program.set(Setting::ShowHelpOnError(true));
+    program.set(Setting::ChoosePredefinedTheme(PredefinedThemes::Colorful));
+    program.set(Setting::SetProgramPattern(Pattern::Legacy));
+    // program.set(Setting::OverrideAllDefaultListeners(true));
+
+    use Color::*;
+    program.set(Setting::DefineCustomTheme(construct_theme!((
+        Green, Magenta, Blue, Red, White
+    ))));
+
+    // program.before_all(|_cfg| {
+    //     println!("Before all");
+    // });
+
+    // program.on(Event::UnknownCommand, |_cfg| {
+    //     println!("Unknown command");
+    // });
 
     // program.parse();
+    // program.parse_from(vec!["image", "ls", "--", "-xc", "-pv"]);
+    // program.parse_from(vec!["-v"]);
     dbg!(program);
+}
+
+fn image_cmd_cb(matches: ParserMatches) {
+    dbg!(matches);
 }
