b615ea27956307d55cf3273128d2a7b667cab761
Victor Ndaba <mwangindaba@gmail.com>
2022-05-09 04:34:23 +0300

    📦 feat: added new program settings

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -10,31 +10,38 @@ pub struct ProgramSettings {
 
 #[allow(unused)]
 #[derive(Debug, Clone)]
-pub(crate) struct NewProgramSettings {
-    pub(crate) show_help_on_error: bool,
+pub(crate) struct InternalSettings {
+    pub(crate) show_help_on_all_errors: bool,
+    pub(crate) show_help_on_empty_args: bool,
     pub(crate) enable_command_suggestions: bool,
     pub(crate) hide_command_aliases: bool,
     pub(crate) events_to_override: Vec<Event>,
     pub(crate) separate_options_and_flags: bool,
     pub(crate) override_all_default_listeners: bool,
+    pub(crate) auto_include_help_subcommand: bool,
+    pub(crate) enable_tree_view_subcommand: bool,
 }
 
-impl Default for NewProgramSettings {
+impl Default for InternalSettings {
     fn default() -> Self {
         Self {
-            show_help_on_error: false,
+            show_help_on_all_errors: false,
+            show_help_on_empty_args: true,
             enable_command_suggestions: true,
             hide_command_aliases: true,
             separate_options_and_flags: false,
             override_all_default_listeners: false,
             events_to_override: vec![],
+            auto_include_help_subcommand: true,
+            enable_tree_view_subcommand: true,
         }
     }
 }
 
 #[allow(unused)]
 pub enum Setting {
-    ShowHelpOnError(bool),
+    ShowHelpOnAllErrors(bool),
+    ShowHelpOnEmptyArgs(bool),
     EnableCommandSuggestion(bool),
     HideCommandAliases(bool),
     SeparateOptionsAndFlags(bool),
@@ -43,6 +50,8 @@ pub enum Setting {
     SetProgramPattern(Pattern),
     OverrideAllDefaultListeners(bool),
     OverrideSpecificEventListener(Event),
+    AutoIncludeHelpSubcommand(bool),
+    EnableTreeViewSubcommand(bool),
 }
 
 impl ProgramSettings {
