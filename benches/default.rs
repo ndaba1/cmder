1f7582ebf315c24b96884981bec68f7e20eb70a6
Victor Ndaba <mwangindaba@gmail.com>
2022-05-09 04:32:10 +0300

    📦 feat: switched benches fully to new api

--- benches/default.rs
+++ benches/default.rs
@@ -1,38 +1,21 @@
+use cmder::core::new_program::Program;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn build_old(c: &mut Criterion) {
-    use cmder::Program;
-    c.bench_function("build old", |b| {
+pub fn build_empty(c: &mut Criterion) {
+    c.bench_function("build empty", |b| {
         b.iter(|| {
-            let program = Program::new();
-
-            // program
-            //     .author("vndaba")
-            //     .bin_name("bench")
-            //     .description("Benchmarks")
-            //     .version("0.1.0");
-
-            program.command("empty");
+            Program::new().subcommand("empty");
         })
     });
 }
 
-pub fn build_new(c: &mut Criterion) {
-    use cmder::core::new_program::Program;
-    c.bench_function("build new", |b| {
+pub fn parse_empty(c: &mut Criterion) {
+    c.bench_function("parse empty", |b| {
         b.iter(|| {
-            let mut program = Program::new();
-
-            // program
-            //     .author("vndaba")
-            //     .bin_name("bench")
-            //     .description("Benchmarks")
-            //     .version("0.1.0");
-
-            program.subcommand("empty");
+            Program::new().parse_from(vec![]);
         })
     });
 }
 
-criterion_group!(benches, build_old, build_new);
+criterion_group!(benches, build_empty, parse_empty);
 criterion_main!(benches);
