fb488c488dd64840a8067fe53cf8233ecc827a97
Victor Ndaba <mwangindaba@gmail.com>
2022-05-11 22:17:35 +0300

    🔃 refactor: updated the custom_theme example

--- examples/custom_theme.rs
+++ examples/custom_theme.rs
@@ -1,4 +1,4 @@
-use cmder::{construct_theme, Color, Program, Theme};
+use cmder::{construct_theme, Color, Program, Setting, Theme};
 
 fn main() {
     let mut program = Program::new();
@@ -10,22 +10,25 @@ fn main() {
 
     program
         .argument("<text...>", "Some dummy text")
-        .action(|values, options| {
-            dbg!(values, options);
+        .action(|m| {
+            dbg!(m);
         });
 
     // You can use the `construct_theme` macro and play around with the colors
     use Color::*;
-    program.set_custom_theme(construct_theme!(Green, Magenta, Blue, Red, White));
+    program.set(Setting::DefineCustomTheme(construct_theme!(
+        Green, Magenta, Blue, Red, White
+    )));
 
     // You could also construct the theme manually
-    program.set_custom_theme(Theme {
+    let theme = Theme {
         keyword: Green,
         headline: Magenta,
         description: Blue,
         error: Red,
         other: White,
-    });
+    };
+    program.set(Setting::DefineCustomTheme(theme));
     // This method will achieve exactly the same outcome as the macro expression above
 
     program.parse();
