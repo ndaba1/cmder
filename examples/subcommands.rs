24a0987bf94057294594693c4ea52ee8596b60d5
Victor Ndaba <mwangindaba@gmail.com>
2022-05-11 23:56:57 +0300

    📦 feat: updated the subcommands example

--- examples/subcommands.rs
+++ examples/subcommands.rs
@@ -1,9 +1,8 @@
 #![allow(unused)]
 
 use cmder::{
-    construct_theme,
-    core::{new_program::Program, Setting},
-    Color, Event, ParserMatches, Pattern, PredefinedThemes, Theme,
+    construct_theme, Color, Event, ParserMatches, Pattern, PredefinedThemes, Theme,
+    {Program, Setting},
 };
 /// This is an example of a program that uses commands which have subcommands themselves. The example used here emulates the docker cli. i.e
 /// docker container ls, docker image ls, docker image build
@@ -15,60 +14,89 @@ fn main() {
         .author("vndaba")
         .description("An example of a program with subcommands");
 
-    // The docker image example command
+    // Bind the newly created subcmd to a variable
     let mut img_cmd = program.subcommand("image");
 
-    // You can then chain the subcommand method then invoke the construct method as the final method.
+    img_cmd
+        .description("A command housing all the subcommands for image functionality")
+        .alias("i");
+
+    // You can then chain the subcommand method to add subcmds to the subcmd.
     img_cmd
         .subcommand("ls")
         .alias("l")
         .option("-l --long", "Use the long listing method")
-        .description("List all the docker images available");
+        .description("List all the docker images available")
+        .action(cmd::img_ls_cb);
 
     img_cmd
         .subcommand("build")
         .alias("b")
         .option("-q --quiet", "Supress output when building")
-        .description("Build a docker image from provided context");
+        .description("Build a docker image from provided context")
+        .action(cmd::img_build_cb);
 
     img_cmd
         .subcommand("prune")
         .alias("p")
         .option("-a --all", "Remove all unused images")
-        .description("Remove the provided image or all unused images");
-
-    // The build method should always be invoked after all the subcommands have been constructed
-    img_cmd
-        .description("A command housing all the subcommands for image functionality")
-        .alias("i");
+        .description("Remove the provided image or all unused images")
+        .action(cmd::img_prune_cb);
 
     // The docker container example command
     let mut cont_cmd = program.subcommand("container");
+
+    cont_cmd
+        .alias("cont")
+        .description("A command housing all subcommands for containers");
 
     cont_cmd
         .subcommand("ls")
         .alias("l")
         .option("-l --long", "Use the long listing method")
         .option("-a --all", "List even stopped conatiners")
-        .description("List all the docker containers available");
+        .description("List all the docker containers available")
+        .action(cmd::cont_ls_cb);
 
     cont_cmd
-        .alias("cont")
-        .description("A command housing all subcommands for containers");
+        .subcommand("start")
+        .alias("s")
+        .option(
+            "-p --port <port-number>",
+            "Add port mappings from container to host",
+        )
+        .option("-i --interactive", "Start container in interactive shell")
+        .option("-d --detached", "Start container in detached state")
+        .action(cmd::cont_start_cb);
 
-    program
-        .subcommand("tree")
-        .argument("<SUB-COMMAND>", "The subcommand to print out the tree for")
-        .description("A subcommand used for printing out a tree view of the command tree")
-        .action(|m| {
-            let cmd = m.get_matched_cmd().unwrap();
-            let val = m.get_arg("<SUB-COMMAND>").unwrap();
-            let parent = cmd.get_parent().unwrap();
+    program.parse();
+}
 
-            if let Some(cmd) = parent.find_subcommand(&val) {
-                cmd.display_commands_tree();
-            }
-        });
+mod cmd {
+    use cmder::ParserMatches;
 
-    program.parse_from(vec!["docker", "tree", "image"]);
+    pub fn img_build_cb(_m: ParserMatches) {
+        // Command logic goes here
+        println!("Image build command called");
+    }
+
+    pub fn img_prune_cb(_m: ParserMatches) {
+        // Command logic goes here
+        println!("Image prune command called");
+    }
+
+    pub fn img_ls_cb(_m: ParserMatches) {
+        // Command logic goes here
+        println!("Image list command called");
+    }
+
+    pub fn cont_start_cb(_m: ParserMatches) {
+        // Command logic goes here
+        println!("Container start command called");
+    }
+
+    pub fn cont_ls_cb(_m: ParserMatches) {
+        // Command logic goes here
+        println!("Container list command called");
+    }
 }
