65e513cff7a55246bdba8dcbfd503e7fe1bae84c
Victor Ndaba <mwangindaba@gmail.com>
2022-05-11 21:35:19 +0300

    📦 feat: added new build_empty_cmd bench

--- benches/default.rs
+++ benches/default.rs
@@ -1,8 +1,16 @@
-use cmder::core::new_program::Program;
+use cmder::{Command, Program};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn build_empty(c: &mut Criterion) {
-    c.bench_function("build empty", |b| {
+pub fn build_empty_cmd(c: &mut Criterion) {
+    c.bench_function("build empty cmd", |b| {
+        b.iter(|| {
+            Command::new("empty");
+        })
+    });
+}
+
+pub fn build_empty_subcmd(c: &mut Criterion) {
+    c.bench_function("build empty subcmd", |b| {
         b.iter(|| {
             Program::new().subcommand("empty");
         })
@@ -12,10 +20,10 @@ pub fn build_empty(c: &mut Criterion) {
 pub fn parse_empty(c: &mut Criterion) {
     c.bench_function("parse empty", |b| {
         b.iter(|| {
-            Program::new().parse_from(vec![]);
+            Program::new().parse_from(vec![""]);
         })
     });
 }
 
-criterion_group!(benches, build_empty, parse_empty);
+criterion_group!(benches, build_empty_cmd, build_empty_subcmd, parse_empty);
 criterion_main!(benches);
