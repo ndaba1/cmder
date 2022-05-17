f3026f5134cda80339f5c9ceae2f4c6ac612fc29
Victor Ndaba <mwangindaba@gmail.com>
2022-05-17 23:00:05 +0300

    📦 feat: added args_building section

--- benches/2_basic.rs
+++ benches/2_basic.rs
@@ -11,5 +11,15 @@ fn build_with_args(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, build_with_args);
+fn build_with_flags(c: &mut Criterion) {
+    c.bench_function("build_with_flags", |b| {
+        b.iter(|| {
+            Command::new("flags")
+                .option("-x --extra", "Something extra")
+                .option("-v --verbose", "Verbosity");
+        })
+    });
+}
+
+criterion_group!(benches, build_with_args, build_with_flags);
 criterion_main!(benches);
