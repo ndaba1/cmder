d02fd86ec6e45a691ca9a1aa04496512c1b0c3e1
Victor Ndaba <mwangindaba@gmail.com>
2022-05-18 23:46:00 +0300

    📦 feat: added standard with subcmds bench

--- benches/3_standard.rs
+++ benches/3_standard.rs
@@ -1,7 +1,7 @@
 use cmder::Program;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn build_standard_with_args(c: &mut Criterion) {
+fn build_std_with_args(c: &mut Criterion) {
     c.bench_function("build_standard_with_args", |b| {
         b.iter(|| {
             let mut program = Program::new();
@@ -20,5 +20,31 @@ pub fn build_standard_with_args(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, build_standard_with_args);
+fn build_std_with_subcmds(c: &mut Criterion) {
+    c.bench_function("build_standard_with_subcmds", |b| {
+        b.iter(|| {
+            let mut program = Program::new();
+
+            program
+                .bin_name("std")
+                .author("vndaba")
+                .version("0.1.0")
+                .description("std");
+
+            program
+                .subcommand("one")
+                .description("first cmd")
+                .alias("first")
+                .argument("<val>", "Some value");
+
+            program
+                .subcommand("two")
+                .description("second cmd")
+                .alias("second")
+                .argument("<val>", "Some value");
+        })
+    });
+}
+
+criterion_group!(benches, build_std_with_args, build_std_with_subcmds);
 criterion_main!(benches);
