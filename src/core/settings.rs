999daa33df566ce4ded6f907720977bcba970e09
Victor Ndaba <mwangindaba@gmail.com>
2022-05-31 14:48:18 +0300

    🔃 refactor: heavy update to the program settings structure

--- src/core/settings.rs
+++ src/core/settings.rs
@@ -1,42 +1,34 @@
-use crate::{Event, Pattern, PredefinedTheme, Theme};
+use std::collections::HashMap;
 
 #[derive(Debug, Clone)]
 pub struct ProgramSettings {
-    pub(crate) ignore_all_errors: bool,
-    pub(crate) hide_command_aliases: bool,
-    pub(crate) show_help_on_all_errors: bool,
-    pub(crate) show_help_on_empty_args: bool,
-    pub(crate) enable_command_suggestions: bool,
-    pub(crate) events_to_override: Vec<Event>,
-    pub(crate) override_all_default_listeners: bool,
-    pub(crate) auto_include_help_subcommand: bool,
+    values: HashMap<Setting, bool>,
+}
+
+impl ProgramSettings {
+    pub fn get(&self, setting: Setting) -> bool {
+        self.values[&setting]
+    }
+
+    pub fn set(&mut self, setting: Setting, val: bool) {
+        self.values.insert(setting, val);
+    }
 }
 
 impl Default for ProgramSettings {
     fn default() -> Self {
         Self {
-            ignore_all_errors: false,
-            show_help_on_all_errors: false,
-            show_help_on_empty_args: true,
-            enable_command_suggestions: true,
-            hide_command_aliases: true,
-            override_all_default_listeners: false,
-            events_to_override: vec![],
-            auto_include_help_subcommand: true,
+            values: HashMap::new(),
         }
     }
 }
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub enum Setting {
-    IgnoreAllErrors(bool),
-    ShowHelpOnAllErrors(bool),
-    ShowHelpOnEmptyArgs(bool),
-    EnableCommandSuggestion(bool),
-    HideCommandAliases(bool),
-    DefineCustomTheme(Theme),
-    ChoosePredefinedTheme(PredefinedTheme),
-    SetProgramPattern(Pattern),
-    OverrideAllDefaultListeners(bool),
-    OverrideSpecificEventListener(Event),
-    AutoIncludeHelpSubcommand(bool),
+    IgnoreAllErrors,
+    ShowHelpOnAllErrors,
+    ShowHelpOnEmptyArgs,
+    ShowCommandAliases,
+    OverrideAllDefaultListeners,
+    AutoIncludeHelpSubcommand,
 }
