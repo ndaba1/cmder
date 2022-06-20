e412f0ae0c6d75f18ac44552e3261e6b34221adb
Victor Ndaba <mwangindaba@gmail.com>
2022-06-20 22:09:35 +0300

    🔃 refactor: removed the explicit lifetime in flag struct

--- src/parse/flags.rs
+++ src/parse/flags.rs
@@ -1,15 +1,15 @@
 use crate::ui::formatter::FormatGenerator;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct CmderFlag<'f> {
+pub struct CmderFlag {
     pub(crate) name: String,
     pub(crate) long: String,
     pub(crate) short: String,
-    pub(crate) description: &'f str,
+    pub(crate) description: String,
     pub(crate) is_global: bool,
 }
 
-impl<'a> CmderFlag<'a> {
+impl<'a> CmderFlag {
     pub fn new(name: &'a str) -> Self {
         let mut long = String::from("--");
         long.push_str(name);
@@ -17,7 +17,7 @@ impl<'a> CmderFlag<'a> {
             name: name.into(),
             short: "".into(),
             long,
-            description: "",
+            description: "".into(),
             is_global: false,
         }
     }
@@ -30,7 +30,7 @@ impl<'a> CmderFlag<'a> {
     }
 
     pub fn help(mut self, val: &'a str) -> Self {
-        self.description = val;
+        self.description = val.into();
         self
     }
 
@@ -40,13 +40,13 @@ impl<'a> CmderFlag<'a> {
     }
 }
 
-impl<'d> Default for CmderFlag<'d> {
+impl<'d> Default for CmderFlag {
     fn default() -> Self {
         Self::new("")
     }
 }
 
-pub(crate) fn resolve_flag<'f>(list: &'f [CmderFlag], val: String) -> Option<CmderFlag<'f>> {
+pub(crate) fn resolve_flag<'f>(list: &'f [CmderFlag], val: String) -> Option<CmderFlag> {
     let mut flag = None;
 
     let val = val.as_str();
@@ -58,18 +58,18 @@ pub(crate) fn resolve_flag<'f>(list: &'f [CmderFlag], val: String) -> Option<Cmd
     flag
 }
 
-impl<'f> FormatGenerator for CmderFlag<'f> {
+impl<'f> FormatGenerator for CmderFlag {
     fn generate(&self, _ptrn: crate::ui::formatter::Pattern) -> (String, String) {
         let short: String = if !self.short.is_empty() {
             format!("{},", self.short)
         } else {
             "  ".into()
         };
-        (format!("{} {}", short, self.long), self.description.into())
+        (format!("{} {}", short, self.long), self.description.clone())
     }
 }
 
-pub(crate) fn new_flag(val: &str, help: &'static str) -> CmderFlag<'static> {
+pub(crate) fn new_flag(val: &str, help: &'static str) -> CmderFlag {
     let values: Vec<_> = val.split_whitespace().collect();
 
     let mut short = "";
@@ -87,7 +87,7 @@ pub(crate) fn new_flag(val: &str, help: &'static str) -> CmderFlag<'static> {
         name: long.replace("--", ""),
         long: long.into(),
         short: short.into(),
-        description: help,
+        description: help.into(),
         is_global: false,
     }
 }
